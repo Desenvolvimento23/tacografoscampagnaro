509050c2cf6636d214d2c6dbbfb64370b58492b9
Campagnaro Truck Center <truckcenter@campagnaro.com.br>
2026-01-16 14:11:27 -0300

    fix: set base path for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: '/tacografoscampagnaro/',
+    base: '/tacografoscampagnaro/',
     assetsInclude: ['**/*.MOV'],
     plugins: [react()],
     resolve: {
